7eb1c8ba9ec47e1998ef72b6f7fa0348a9ae838e
Mate Kiss <Mate_Kiss@epam.com>
2024-06-07 23:13:25 +0200

    Solution

---
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.task03;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "hello_world",
+	roleName = "hello_world-role",
+	isPublishVersion = true,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
+		return resultMap;
+	}
+}
